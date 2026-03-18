9c3f1c2b3e9e0198aa6383b63d543afa2170f711
gowri-7 <166044015+gowri-7@users.noreply.github.com>
2026-03-18 11:52:53 +0530

    Implement logic to find largest element and validate

--- src/LargestTwice.java
+++ src/LargestTwice.java
@@ -1,9 +1,40 @@
+
+import java.util.Scanner;
+
 public class LargestTwice {
-    
-        // TODO: Read n
-        // TODO: Read array elements
-        // TODO: Find the largest element and its index
-        // TODO: Check if largest >= 2 * every other element
-        // TODO: Print index or -1
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int max = nums[0];
+        int index = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                index = i;
+            }
+        }
+
+        boolean valid = true;
+
+        for (int i = 0; i < n; i++) {
+            if (i != index && max < 2 * nums[i]) {
+                valid = false;
+                break;
+            }
+        }
+
+        if (valid) {
+            System.out.println(index);
+        } else {
+            System.out.println(-1);
+        }
+    }
 }
